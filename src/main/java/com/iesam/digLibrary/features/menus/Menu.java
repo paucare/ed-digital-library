0d0b1a77c1d70c40d3707835f37f9bc748d9b961
Alessandro Paucar <146753377+paucare@users.noreply.github.com>
2024-05-09 00:00:54 +0200

    feature: MenuSystem implemented (#13)

---
+++ src/main/java/com/iesam/digLibrary/features/menus/Menu.java
@@ -0,0 +1,6 @@
+package com.iesam.digLibrary.features.menus;
+
+public abstract class Menu {
+    abstract void display();
+    abstract void processChoice(int choice);
+}
---
+++ src/main/java/com/iesam/digLibrary/features/menus/ResourcesMenu.java
@@ -0,0 +1,45 @@
+package com.iesam.digLibrary.features.menus;
+
+import com.iesam.digLibrary.features.resources.presentation.ResourcesPresentation;
+
+public class ResourcesMenu extends Menu {
+    public final MenuSystem menuSystem;
+
+    public ResourcesMenu(MenuSystem menuSystem) {
+        this.menuSystem = menuSystem;
+    }
+
+    @Override
+    void display() {
+        System.out.println("R. Gestion de recursos .R");
+        System.out.println("0. Volver al menu principal");
+        System.out.println("1. Dar de alta a un recurso");
+        System.out.println("2. Dar de baja un recurso");
+        System.out.println("3. Mostrar lista de recursos");
+        System.out.print("Introduce una opcion: ");
+    }
+
+    @Override
+    void processChoice(int choice) {
+        switch (choice) {
+            case 0:
+                System.out.println("Volver al menu principal");
+                menuSystem.goBack();
+                break;
+            case 1:
+                ResourcesPresentation.showResourceForm();
+                System.out.println("Siguiente operación");
+                return;
+            case 2:
+                ResourcesPresentation.deleteResourceById();
+                System.out.println("Siguiente operación");
+                return;
+            case 3:
+                ResourcesPresentation.getAllRegisters();
+                System.out.println("Siguiente operación");
+            default:
+                System.out.println("Opcion no valida. Intentalo de nuevo.");
+                break;
+        }
+    }
+}
