b8e5aebfcfa80efc539426ea13d07535836bd78f
Alessandro Paucar <apaucare27@gmail.com>
2024-05-15 23:52:44 +0200

    test: build.gradle.kts updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,10 @@ dependencies {
     implementation ("com.google.code.gson:gson:2.10.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    //Mockito framework
+    testImplementation("org.mockito:mockito-core:5.11.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.11.0")
+
 
 }
 
