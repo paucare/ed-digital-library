3d06038a98dd19a13298082c25d3e5d8c6fbdfa5
Alessandro Paucar <146753377+paucare@users.noreply.github.com>
2024-05-19 23:57:54 +0200

    UserDatarRepository testing (#39)
    * chore: EndLoanUseCase code improved
    
    * feature: validation of id in three use cases
    
    * test: UserDataRepository testing implemented
    
    * test: tested correctly the dataRepository

--- src/main/java/com/iesam/digLibrary/features/user/domain/SaveUserUseCase.java
+++ src/main/java/com/iesam/digLibrary/features/user/domain/SaveUserUseCase.java
@@ -1,5 +1,7 @@
 package com.iesam.digLibrary.features.user.domain;
 
+import com.iesam.digLibrary.features.exceptions.NotValidInputException;
+
 public class SaveUserUseCase {
     public final UserRepository repository;
 
@@ -8,6 +10,11 @@ public class SaveUserUseCase {
     }
 
     public void execute(User user) {
-        repository.saveUser(user);
+        User existingUser = repository.getUserById(user.dni);
+        if(existingUser == null){
+            repository.saveUser(user);
+        } else {
+            throw  new NotValidInputException("A user with said ID already exists");
+        }
     }
 }
--- src/main/java/com/iesam/digLibrary/features/user/presentation/UserPresentation.java
+++ src/main/java/com/iesam/digLibrary/features/user/presentation/UserPresentation.java
@@ -1,5 +1,6 @@
 package com.iesam.digLibrary.features.user.presentation;
 
+import com.iesam.digLibrary.features.exceptions.NotValidInputException;
 import com.iesam.digLibrary.features.user.data.*;
 import com.iesam.digLibrary.features.user.data.local.UserFileLocalDataSource;
 import com.iesam.digLibrary.features.user.domain.*;
@@ -11,52 +12,64 @@ public class UserPresentation {
 
     public static void showUserForm(){
 
-        System.out.println("Formulario de nuevo usuario");
-        System.out.println("Introduce el dni: ");
+        System.out.println("New user form");
+        System.out.println("Enter the ID: ");
         String dni = sc.nextLine();
-        System.out.println("Introduce el nombre: ");
+        System.out.println("Enter the name: ");
         String name = sc.nextLine();
-        System.out.println("Introduce el apellido: ");
+        System.out.println("Enter the surname: ");
         String surname = sc.nextLine();
-        System.out.println("Introduce el telefono: ");
+        System.out.println("Enter the phone number: ");
         int phoneNumber = sc.nextInt();
         sc.nextLine();
-        System.out.println("Introduce la dirección: ");
+        System.out.println("Enter the address: ");
         String address = sc.nextLine();
 
         User user = new User(dni,name,surname,phoneNumber,address);
         SaveUserUseCase useCase = new SaveUserUseCase(new UserDataRepository(new UserFileLocalDataSource()));
-        useCase.execute(user);
-        System.out.println("Se ha guardado el usuario: " + user.name);
+        try{
+            useCase.execute(user);
+            System.out.println("The user : " + user.name + " was saved");
+        } catch (NotValidInputException e) {
+            e.getMessage();
+        }
     }
 
     public static void deleteUserByDni(){
-        System.out.println("Introduce el dni del usuario a borrar: ");
+        System.out.println("Enter the users' ID to delete: ");
         String dni = sc.nextLine();
         DeleteUserUseCase useCase = new DeleteUserUseCase(new UserDataRepository(new UserFileLocalDataSource()));
-        useCase.execute(dni);
-        System.out.println("Se ha borrado el usuario con dni " + dni + " con exito");
+        try{
+            useCase.execute(dni);
+            System.out.println("The user with ID: " + dni + " was deleted successfully");
+        } catch (NotValidInputException e){
+            e.getMessage();
+        }
     }
 
     public static void updateUser(){
 
-            System.out.println("Introduce el ID del usuario a modificar");
+            System.out.println("Enter the user's ID to delete");
             String unchangedId = sc.nextLine();
 
-            System.out.println("Formulario para editar datos de usuario");
-            System.out.println("Introduce el nombre: ");
+            System.out.println("Edit user data form");
+            System.out.println("Enter the name: ");
             String name = sc.nextLine();
-            System.out.println("Introduce el apellido: ");
+            System.out.println("Enter the surname: ");
             String surname = sc.nextLine();
-            System.out.println("Introduce el telefono: ");
+            System.out.println("Enter the phone number: ");
             int phoneNumber = sc.nextInt();
             sc.nextLine();
-            System.out.println("Introduce la dirección: ");
+            System.out.println("Enter the address: ");
             String address = sc.nextLine();
             User updatedUser = new User(unchangedId,name,surname,phoneNumber,address);
             UpdateUserUseCase useCase = new UpdateUserUseCase(new UserDataRepository(new UserFileLocalDataSource()));
-            useCase.execute(updatedUser);
-            System.out.println("");
+            try {
+                useCase.execute(updatedUser);
+            }catch (NotValidInputException e){
+                e.getMessage();
+            }
+
 
     }
 
