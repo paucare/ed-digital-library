449daed9a50ca6ab6bc568c40f135434b87a4b7c
Alessandro Paucar <146753377+paucare@users.noreply.github.com>
2024-05-21 19:38:48 +0200

    feat: extra logic implemented

--- src/main/java/com/iesam/digLibrary/features/user/data/UserDataRepository.java
+++ src/main/java/com/iesam/digLibrary/features/user/data/UserDataRepository.java
@@ -5,6 +5,7 @@ import com.iesam.digLibrary.features.user.data.local.UserMemLocalDataSource;
 import com.iesam.digLibrary.features.user.domain.User;
 import com.iesam.digLibrary.features.user.domain.UserRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDataRepository implements UserRepository {
@@ -33,38 +34,27 @@ public class UserDataRepository implements UserRepository {
 
     @Override
     public User getUserById(String id) {
-        User memUser = UserMemLocalDataSource.getInstance().findById(id);
-        User fileUser = localDataSource.findById(id);
 
-        if (memUser != null) {
-            return memUser;
+        User user = UserMemLocalDataSource.getInstance().findById(id);
 
-        } else if(fileUser!=null){
-            UserMemLocalDataSource.getInstance().save(fileUser);
-            return fileUser;
-
-        } else {
-            System.out.println("Data not found");
-            return null;
+        if(user==null){
+            user = localDataSource.findById(id);
+            UserMemLocalDataSource.getInstance().save(user);
         }
+        return user;
     }
 
     @Override
     public List<User> getAllUsers() {
-        List<User> memUserList =  UserMemLocalDataSource.getInstance().findAll();
 
-        if(memUserList!= null) {
-            return memUserList;
+        List<User> userList =  UserMemLocalDataSource.getInstance().findAll();
 
-        } else if(!localDataSource.findAll().isEmpty()){
-            List<User> fileUserList = localDataSource.findAll();
-            for ( User element: fileUserList){
+        if(userList.isEmpty()) {
+            userList = localDataSource.findAll();
+            for ( User element: userList){
                 UserMemLocalDataSource.getInstance().save(element);
             }
-            return fileUserList;
-        } else {
-            System.out.println("Data not found");
-            return null;
         }
+            return userList;
     }
 }
