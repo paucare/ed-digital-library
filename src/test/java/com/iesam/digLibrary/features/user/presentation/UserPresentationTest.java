7bcf723d379b9adc08617b9bc4fb46a7d9bc2fd0
Alessandro Paucar <146753377+paucare@users.noreply.github.com>
2024-05-23 18:30:44 +0200

    feat: presentation testing implemented

--- src/test/java/com/iesam/digLibrary/features/user/presentation/UserPresentationTest.java
+++ src/test/java/com/iesam/digLibrary/features/user/presentation/UserPresentationTest.java
@@ -1,7 +1,6 @@
 package com.iesam.digLibrary.features.user.presentation;
 
 import com.iesam.digLibrary.features.user.domain.SaveUserUseCase;
-import com.iesam.digLibrary.features.user.domain.User;
 import com.iesam.digLibrary.features.user.domain.UserRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,9 +15,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 class UserPresentationTest {
@@ -50,19 +46,19 @@ class UserPresentationTest {
 
         Mockito.when(factory.buildSaveUser()).thenReturn(useCase);
 
-        String simulatedInput = "123\nBenito\nMartinez\n123456789\nAdress\n";
+        String simulatedInput = "123\nBenito Antonio\nMartinez Ocasio\n123456789\nAdress\n";
 
+        //Capturamos la entrada
         InputStream in = new ByteArrayInputStream(simulatedInput.getBytes());
         System.setIn(in);
         presentation.sc = new Scanner(System.in);
 
+        //Capturamos la salida
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
-        // Execute the method
         presentation.showUserForm();
 
-        // Verify that buildSaveUser was called once
         Mockito.verify(factory, Mockito.times(1)).buildSaveUser();
     }
 }
